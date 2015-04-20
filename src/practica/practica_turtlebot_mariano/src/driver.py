2702b81ba1a7257bee906ea2e05ac5f9e81791cd
maru <maru@maru-VirtualBox.(none)>
2015-04-20 12:58:00 -0400

    diversos cambios.

--- src/practica/practica_turtlebot_mariano/src/driver.py
+++ src/practica/practica_turtlebot_mariano/src/driver.py
@@ -8,65 +8,98 @@ import rospy
 # The velocity command message
 from geometry_msgs.msg import Twist
 
-# The laser scan message
-from sensor_msgs.msg import LaserScan
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+import actionlib
+from actionlib_msgs.msg import *
 
 # We use a hyperbolic tangent as a transfer function
 from math import tanh
 import math
 
-class driver:
-    def __init__(self, distance, max_speed=0.2, min_speed=0.01):
-        # How close should we get to things, and what's our maximum speed?
-        self.distance = distance
-        self.max_speed = max_speed
-        self.min_speed = min_speed
+class Point:
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
 
-        # Subscriber for the laser data
-        self.sub = rospy.Subscriber('scan', LaserScan, self.laser_callback)
+class Driver:
+    def __init__(self, start_position, end_position, max_speed=0.2, rate=10):
+        self.start_position = start_position
+        self.current_position = start_position
+        self.end_position = end_position
+        self.max_speed = max_speed
+        self.rate = rate
+
+
+
+        # Subscriber for the encoder data
+        # When data of type LaserScal arrives from topic 'scan' call laser_callback function immediately
+        # self.sub = rospy.Subscriber('scan', LaserScan, self.laser_callback)
 
         # Publisher for movement commands
-        self.pub = rospy.Publisher("/mobile_base/commands/velocity",Twist)
+        # We publish data of type Twist in velocity topic
+        self.cmd_vel = rospy.Publisher("/mobile_base/commands/velocity", Twist)
+        # Set refresh rate
 
         # Let the world know we're ready
-        rospy.loginfo('Stoper initialized')
-        rospy.loginfo('Distance aturada:'+ str(self.distance))
+        # rospy.loginfo('Driver initialized')
+        # rospy.loginfo('Start position: '+ str(self.current_position))
+        # rospy.loginfo('End position: '+ str(self.end_position))
+        # What function to call when you ctrl + c    
+        rospy.on_shutdown(self.shutdown)
+
+    def shutdown(self):
+        # stop turtlebot
+        rospy.loginfo("Stop TurtleBot")
+        # a default Twist has linear.x of 0 and angular.z of 0.  So it'll stop TurtleBot
+        self.cmd_vel.publish(Twist())
+        # sleep just makes sure TurtleBot receives the stop command prior to shutting down the script
+        rospy.sleep(1)
+
+    def go_forward(self, speed = 0.2, rotation = 0):
+        r = rospy.Rate(self.rate);
+
+        move_cmd = Twist()
+        # let's go forward at 0.2 m/s
+        move_cmd.linear.x = speed
+        # let's turn at 0 radians/s
+        move_cmd.angular.z = rotation
+
+        # as long as you haven't ctrl + c keeping doing...
+        while not rospy.is_shutdown():
+            # publish the velocity
+            self.cmd_vel.publish(move_cmd)
+            # wait for 0.1 seconds (10 HZ) and publish again
+            r.sleep()
+
+    def go_forward_avoid_obstacles(self):
+        #tell the action client that we want to spin a thread by default
+        self.move_base = actionlib.SimpleActionClient("move_base", MoveBaseAction)
+        rospy.loginfo("wait for the action server to come up")
+        #allow up to 5 seconds for the action server to come up
+        self.move_base.wait_for_server(rospy.Duration(5))
+
+        #we'll send a goal to the robot to move 3 meters forward
+        goal = MoveBaseGoal()
+        goal.target_pose.header.frame_id = 'base_link'
+        goal.target_pose.header.stamp = rospy.Time.now()
+        goal.target_pose.pose.position.x = 3.0 #3 meters
+        goal.target_pose.pose.orientation.w = 1.0 #go forward
+
+        #start moving
+        self.move_base.send_goal(goal)
+
+        #allow TurtleBot up to 60 seconds to complete task
+        success = self.move_base.wait_for_result(rospy.Duration(60)) 
 
 
-
-    def laser_callback(self, scan):
-        # What's the closest laser reading
-        closest = min(scan.ranges)
-        rospy.loginfo('Llegint dades')
-
-        # This is the command we send to the robot
-        command = Twist()
-
-        # If we're much more than 50cm away from things, then we want
-        # to be going as fast as we can.  Otherwise, we want to slow
-        # down.  A hyperbolic tangent transfer function will do this
-        # nicely.
-
-        if self.distance < closest: # or math.isnan(closest):
-            command.linear.x = self.max_speed
-            command.angular.z = 0.0
+        if not success:
+                    self.move_base.cancel_goal()
+                    rospy.loginfo("The base failed to move forward 3 meters for some reason")
         else:
-            command.linear.y =0.0
-            command.angular.z = 2.5
+            # We made it!
+            state = self.move_base.get_state()
+            if state == GoalStatus.SUCCEEDED:
+                rospy.loginfo("Hooray, the base moved 3 meters forward")
 
-        
-        command.linear.y = 0.0
-        command.linear.z = 0.0
-        command.angular.x = 0.0
-        command.angular.y = 0.0
-        #command.angular.z = 0.0
 
-        
-        # If we're going too slowly, then just stop
-        if abs(command.linear.x) < self.min_speed:
-            command.linear.x = 0
-
-        rospy.loginfo('Distance: {0}, speed: {1}'.format(closest, command.linear.x))
-
-        # Send the command to the motors
-        self.pub.publish(command) 
--- src/practica/practica_turtlebot_mariano/src/node.py
+++ src/practica/practica_turtlebot_mariano/src/node.py
@@ -4,18 +4,24 @@
 import roslib; roslib.load_manifest('practica_turtlebot')
 import rospy
 
-from driver import driver
+from driver import Driver
+from driver import Point
 
 if __name__ == '__main__':
     try:
         # Starts a unique node with name driver
         rospy.init_node('driver')
 
-        # Get stoper distance threshold from parameter server.  Default is 0.5
-        stop_distance = rospy.get_param('distance', 0.5)
+        # Get current position
+        start_position = Point(0,0,0)
+        end_position = Point(0,0,0)
 
-        # Set up the controller
-        stopper = driver(stop_distance)
+        # Create driver
+        driver = Driver(start_position, end_position)
+
+        # Tell him what to do
+        driver.go_forward_avoid_obstacles()
+
 
         # Hand control over to ROS
         # This function will only exit when the user press Ctrl + C
