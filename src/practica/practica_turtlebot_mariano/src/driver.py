cf621b716c23ed5b11b81172e257cb6afd37d4f3
Sharekhan <u1910541@correu.udg.edu>
2015-04-27 15:02:24 -0400

    Add impl. of stop on obstacle

--- src/practica/practica_turtlebot_mariano/src/driver.py
+++ src/practica/practica_turtlebot_mariano/src/driver.py
@@ -8,6 +8,9 @@ import rospy
 # The velocity command message
 from geometry_msgs.msg import Twist
 
+# The laser scan message
+from sensor_msgs.msg import LaserScan
+
 # We use a hyperbolic tangent as a transfer function
 from math import tanh
 import math
@@ -19,21 +22,21 @@ class Point:
       self.z = z
 
 class Driver:
-    def __init__(self, start_position, end_position, max_speed=0.2, rate=10):
+    def __init__(self, start_position, end_position, rate=10):
       self.start_position = start_position
       self.current_position = start_position
       self.end_position = end_position
-      self.max_speed = max_speed
       self.rate = rate
+      self.obstacle = False
+      self.obstacle_threshold = 0.5
 
       # Subscriber for the encoder data
       # When data of type LaserScal arrives from topic 'scan' call laser_callback function immediately
-      # self.sub = rospy.Subscriber('scan', LaserScan, self.laser_callback)
+      self.sub = rospy.Subscriber('scan', LaserScan, self.laser_callback) # self.sub.unregister()
 
       # Publisher for movement commands
       # We publish data of type Twist in velocity topic
       self.cmd_vel = rospy.Publisher("/mobile_base/commands/velocity", Twist)
-      # Set refresh rate
 
       # Let the world know we're ready
       # rospy.loginfo('Driver initialized')
@@ -42,66 +45,83 @@ class Driver:
       # What function to call when you ctrl + c    
       rospy.on_shutdown(self.shutdown)
 
-    def bug_0(self, turn_orientation = 'Left', accepted_error = 1):
-      rospy.loginfo('Starting bug_0 algorithm')
-      rospy.loginfo('Turning orientation is ' + turn_orientation)
-      rospy.loginfo('Rate is ' + self.rate)
+    # def bug_0(self, turn_orientation = 'Left', accepted_error = 1):
+    #   rospy.loginfo('Starting bug_0 algorithm')
+    #   rospy.loginfo('Turning orientation is ' + turn_orientation)
+    #   rospy.loginfo('Rate is ' + self.rate)
 
-      r = rospy.Rate(self.rate)
+    #   r = rospy.Rate(self.rate)
 
-      goal_reached = false
-      while not goal_reached:
-        head_toward_goal()
-        if not is_obstacle():
-          go_forward()
-        else:
-          avoid_obstacle(turn_orientation)
-          go_forward()
-        goal_reached = is_goal(accepted_error)
-        r.sleep()
-      rospy.loginfo('Congratulations!!! Goal reached')
+    #   goal_reached = false
+    #   while not goal_reached:
+    #     head_toward_goal()
+    #     if not is_obstacle():
+    #       go_forward()
+    #     else:
+    #       avoid_obstacle(turn_orientation)
+    #       go_forward()
+    #     goal_reached = is_goal(accepted_error)
+    #     r.sleep()
+    #   rospy.loginfo('Congratulations!!! Goal reached')
 
     # Turn the robot facing the goal
-    def head_toward_goal(self):
+    #def head_toward_goal(self):
+
+    #def turn_on_obstacle(self):
+
+    def stop_on_obstacle(self):
+      rospy.loginfo('Starting stop_on_obstacle')
+      r = rospy.Rate(self.rate)
+      while not self.is_obstacle():
+        self.go_forward()
+        r.sleep()
+      rospy.loginfo('Stopping stop_on_obstacle')
+      self.stop()
 
     # Return true if there is a obstacle in the forward direction. False otherwise.
     def is_obstacle(self):
+      if self.obstacle:
+        rospy.loginfo('Obstacle found!!!')
+      return self.obstacle
+
+    def laser_callback(self, scan):
+      closest = min(scan.ranges)
+      self.obstacle = self.obstacle_threshold >= closest or (math.isnan(closest) and self.obstacle)
+      rospy.loginfo('Reading data from scanner, Distance: {0}'.format(closest))
 
     # Move the robot in the forward direction
-    def go_forward(self, speed = 0.2):
+    def go_forward(self, speed = 0.5):
+      rospy.loginfo('Moving forward at  Speed : {0}'.format(speed))
       twist_forward = Twist()
-      # let's go forward at 0.2 m/s
+      # let's go forward at 0.5 m/s
       twist_forward.linear.x = speed
-      # publish the velocity
+      # publish the command
       self.cmd_vel.publish(twist_forward)
 
+    # def turn(self, turn_speed = 0.1):
+    #   twist_turn = Twist()
+    #   # let's go forward at 0.2 m/s
+    #   twist_turn.angular.z = turn_speed
+    #   # publish the command
+    #   self.cmd_vel.publish(twist_forward)
+
     # Turn the robot with turn_orientation until he stops facing a obstacle 
-    def avoid_obstacle(self, turn_orientation):
+    # def avoid_obstacle(self, turn_orientation, turn_rate = 0.1):
+    #   while not is_obstacle():
+    #     turn(turn_orientation, turn_rate)
 
     # Return true if the robot has reached the goal with the given accepted error. False otherwise.
-    def is_goal(self, accepted_error):
-      # Math.hypot(current position, end position)
+    # def is_goal(self, accepted_error):
+    #   # Math.hypot(current position, end position)
 
-    # def go_forward(self, speed = 0.2, rotation = 0):
-    #   r = rospy.Rate(self.rate);
-
-    #   move_cmd = Twist()
-    #   # let's go forward at 0.2 m/s
-    #   move_cmd.linear.x = speed
-    #   # let's turn at 0 radians/s
-    #   move_cmd.angular.z = rotation
-
-    #   # as long as you haven't ctrl + c keeping doing...
-    #   while not rospy.is_shutdown():
-    #       # publish the velocity
-    #       self.cmd_vel.publish(move_cmd)
-    #       # wait for 0.1 seconds (10 HZ) and publish again
-    #       r.sleep()
+    def stop(self):
+      rospy.loginfo('Stopping')
+      twist_stop = Twist()
+      # publish the command
+      self.cmd_vel.publish(twist_stop)
 
     def shutdown(self):
-      # stop turtlebot
-      rospy.loginfo("Stop TurtleBot")
-      # a default Twist has linear.x of 0 and angular.z of 0.  So it'll stop TurtleBot
-      self.cmd_vel.publish(Twist())
-      # sleep just makes sure TurtleBot receives the stop command prior to shutting down the script
+      rospy.loginfo("Shutting down TurtleBot")
+      self.stop()
       rospy.sleep(1)
+      rospy.signal_shutdown("Shutdown function has been called")
