5b7db97d615b9e30a2780a45bd1c38690e6e2e51
Sharekhan <u1910541@correu.udg.edu>
2015-04-27 15:03:02 -0400

    Add impl. of stop on obstacle

--- src/practica/practica_turtlebot_mariano/src/node.py
+++ src/practica/practica_turtlebot_mariano/src/node.py
@@ -8,26 +8,23 @@ from driver import Driver
 from driver import Point
 
 if __name__ == '__main__':
-    try:
-        # Starts a unique node with name driver
-        rospy.init_node('driver')
+  try:
+    # Starts a unique node with name driver
+    rospy.init_node('driver')
 
-        # Get current position
-        start_position = Point(0,0,0)
-        end_position = Point(0,0,0)
+    # Get current position
+    start_position = Point(0,0,0)
+    end_position = Point(0,0,0)
 
-        # Create driver
-        driver = Driver(start_position, end_position)
+    # Create driver
+    driver = Driver(start_position, end_position)
 
-        # Tell him what to do
-        driver.go_forward()
+    # Tell him what to do
+    # driver.bug_0()
+    driver.stop_on_obstacle()
 
-
-        # Hand control over to ROS
-        # This function will only exit when the user press Ctrl + C
-        # Does not do anything. Only handles here the program
-        rospy.spin() 
-
-            
-    except rospy.ROSInterruptException:
-        pass
+    # Hand control over to ROS
+    # This function will only exit when the user press Ctrl + C
+    # Does not do anything. Only handles here the program     
+  except rospy.ROSInterruptException:
+    pass
