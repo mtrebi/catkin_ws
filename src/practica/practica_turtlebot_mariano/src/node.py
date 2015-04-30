5d08a8524a23f553ca528b6ecde74e81626f74e0
maru <maru@maru-VirtualBox.(none)>
2015-04-30 06:42:27 -0400

    Full impl. of bug_0. Must fix is_heading_goal.

--- src/practica/practica_turtlebot_mariano/src/node.py
+++ src/practica/practica_turtlebot_mariano/src/node.py
@@ -5,20 +5,31 @@ import roslib; roslib.load_manifest('practica_turtlebot')
 import rospy
 
 from driver import Driver
+from geometry_msgs.msg import Pose
 
 if __name__ == '__main__':
   try:
     # Starts a unique node with name driver
     rospy.init_node('driver')
 
+    # Create start_pose
+    start_pose = Pose()
+    start_pose.position.x = 0
+    start_pose.position.y = 0
+
+    # Create end_pose
+    end_pose = Pose()
+    end_pose.position.x = 1
+    end_pose.position.y = 0
+
     # Create driver
-    driver = Driver()
+    driver = Driver(start_pose, end_pose)
 
     # Tell him what to do
     # driver.stop_on_obstacle()
-    driver.turn_on_obstacle()
+    # driver.turn_on_obstacle()
     # driver.bug_0()
-
+    driver.head_toward_goal()
     # Hand control over to ROS
     # This function will only exit when the user press Ctrl + C
     # Does not do anything. Only handles here the program     
